d2995348053fdbb847625da2c36faf3c5c27de16
Stryzhevskyi <stryzhevsky.s@gmail.com>
2016-01-22 16:25:00 +0200

    v0.3.6 Fix for IE < 10  #5

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,17 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var jshint = require('gulp-jshint');
-var watch = require('gulp-watch');
+var connect = require('gulp-connect');
+
 
 var dir = {
     src: {
-        cssMin : 'rangeSlider.min.css',
+        cssMin: 'rangeSlider.min.css',
         css: './src/*.css',
         js: './src/*.js'
     },
     dist: {
-        jsMin : 'rangeSlider.min.js',
+        jsMin: 'rangeSlider.min.js',
         css: './dist/',
         js: './dist/'
     }
@@ -53,11 +54,17 @@ gulp.task('jshint', function () {
 });
 
 gulp.task('watch', function () {
-    watch(dir.src.js, function () {
-        gulp.start('js');
-    });
-    watch(dir.src.css, function () {
-        gulp.start('css');
+    gulp.watch(dir.src.js, ['js']);
+    gulp.watch(dir.src.css, ['css']);
+});
+
+gulp.task('server', ['watch'], function () {
+    return connect.server({
+        port: 8000,
+        host: '0.0.0.0',
+        livereload: {
+            port: 35730
+        }
     });
 });
 
@@ -66,5 +73,6 @@ gulp.task('build', ['js', 'css', 'jshint']);
 gulp.task('default', [
     'js',
     'css',
-    'watch'
+    'watch',
+    'server'
 ]);
